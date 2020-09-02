c432ea252641865689e5f16517f03d69c55c8453
王世桢 <wangsz5@aisainfo.com>
2020-09-03 02:05:51 +0800

    图片压缩工具集成&&ts改造

--- src/function/ZipImg/index.ts
+++ src/function/ZipImg/index.ts
@@ -4,37 +4,42 @@ import { MegaPixImage } from "./MegaPixImage";
 /**
  * @description 图片压缩的最核心的步骤——压缩
  * @param image Image对象 dom
- * @param maxC 设置压缩的图片的最长边
+ * @param options.max_width 设置压缩的图片的最长边
+ * @param options.quality 压缩质量
  */
-export function ZipImg(image: HTMLImageElement, maxC = 750) {
+export function ZipImg(
+  image: HTMLImageElement,
+  options = { max_width: 750, quality: 0.8 }
+) {
+  const { max_width, quality } = options;
   const canvas = document.createElement("canvas");
-  let Cwidth = image.width;
-  let Cheight = image.height;
-  let bl;
+  let c_width = image.width;
+  let c_height = image.height;
+  let bl: number;
 
-  if (Cwidth > 300 && Cheight > 300) {
-    if (Cwidth > Cheight) {
-      bl = Cwidth / Cheight;
-      Cwidth = maxC;
-      Cheight = Cwidth / bl;
+  if (c_width > 300 && c_height > 300) {
+    if (c_width > c_height) {
+      bl = c_width / c_height;
+      c_width = max_width;
+      c_height = c_width / bl;
     } else {
-      bl = Cheight / Cwidth;
-      Cheight = maxC;
-      Cwidth = Cheight / bl;
+      bl = c_height / c_width;
+      c_height = max_width;
+      c_width = c_height / bl;
     }
   }
 
-  canvas.width = Cwidth;
-  canvas.height = Cheight;
+  canvas.width = c_width;
+  canvas.height = c_height;
 
   // const context = canvas.getContext("2d");
-  // context.drawImage(imageObj, 0, 0, Cwidth, Cheight);
+  // context.drawImage(imageObj, 0, 0, c_width, c_height);
 
   const mpimg = new MegaPixImage(image);
-  mpimg.render(canvas, { width: Cwidth, height: Cheight });
+  mpimg.render(canvas, { width: c_width, height: c_height });
 
-  // const imgFile = canvas.toDataURL("image/jpeg", 0.7);
-  const imgFile = canvas.toDataURL("image/png");
-  // let imgFile = canvas.toDataURL("image/webp");
+  // const imgFile = canvas.toDataURL("image/jpeg", quality);
+  const imgFile = canvas.toDataURL("image/png", quality);
+  // let imgFile = canvas.toDataURL("image/webp",quality);
   return Base64ToFile(imgFile);
 }
