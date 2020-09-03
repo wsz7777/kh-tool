68796585cce24e4c1f4de1c356bcd9a7ce8d9367
王世桢 <wangsz5@aisainfo.com>
2020-09-03 19:36:37 +0800

    zipImg 增加格式设定

--- src/function/ZipImg/index.ts
+++ src/function/ZipImg/index.ts
@@ -1,11 +1,15 @@
 import { Base64ToFile } from "../../base/Base64ToFile";
 import { MegaPixImage } from "./MegaPixImage";
 
+type ImageType = "image/jpeg" | "image/png" | "image/webp";
+
 interface ZipOptions {
   /** 最长边的长度 */
   max_length?: number;
   /** 压缩质量 0 ~ 1 之间*/
   quality?: number;
+  /** 图片类型 */
+  type?: ImageType;
 }
 
 /**
@@ -15,7 +19,7 @@ interface ZipOptions {
  * @param options.quality 压缩质量
  */
 export function MinImg(image: HTMLImageElement, options: ZipOptions = {}) {
-  const { max_length, quality } = options;
+  const { max_length, quality, type = "image/jpeg" } = options;
   const canvas = document.createElement("canvas");
   let c_width = image.width;
   let c_height = image.height;
@@ -37,15 +41,12 @@ export function MinImg(image: HTMLImageElement, options: ZipOptions = {}) {
   canvas.width = c_width;
   canvas.height = c_height;
 
-  // const context = canvas.getContext("2d");
-  // context.drawImage(imageObj, 0, 0, c_width, c_height);
-
+  // const mpimg = canvas.getContext("2d");
+  // mpimg.drawImage(imageObj, 0, 0, c_width, c_height);
   const mpimg = new MegaPixImage(image);
-  mpimg.render(canvas, { width: c_width, height: c_height, quality });
+  mpimg.render(canvas, { width: c_width, height: c_height, quality, type });
 
-  // const imgFile = canvas.toDataURL("image/jpeg", quality);
-  const imgFile = canvas.toDataURL("image/png", quality);
-  // let imgFile = canvas.toDataURL("image/webp",quality);
+  const imgFile = canvas.toDataURL(type, quality);
   const getZipRstFile = Base64ToFile(imgFile);
   if (getZipRstFile instanceof File || getZipRstFile instanceof Blob) {
     return getZipRstFile;
--- test/base/IdRandom.test.ts
+++ test/base/IdRandom.test.ts
@@ -6,25 +6,24 @@ describe("NoEmptyArr:", () => {
       .fill(0)
       .map(() => IdRandom());
     const set = [...new Set(arr)];
-    console.log("arr.length === set.length", arr.length === set.length);
     expect(arr.length === set.length).toBe(true);
   });
 
-  // test("1000000", () => {
-  //   const arr = Array(1000000)
-  //     .fill(0)
-  //     .map(() => IdRandom());
-  //   const set = [...new Set(arr)];
-  //   console.log("arr.length === set.length", arr.length === set.length);
-  //   expect(arr.length === set.length).toBe(true);
-  // });
-  
-  // test("10000000", () => {
-  //   const arr = Array(10000000)
-  //     .fill(0)
-  //     .map(() => IdRandom());
-  //   const set = [...new Set(arr)];
-  //   console.log("arr.length === set.length", arr.length === set.length);
-  //   expect(arr.length === set.length).toBe(true);
-  // });
+  // // test("1000000", () => {
+  // //   const arr = Array(1000000)
+  // //     .fill(0)
+  // //     .map(() => IdRandom());
+  // //   const set = [...new Set(arr)];
+  // //   console.log("arr.length === set.length", arr.length === set.length);
+  // //   expect(arr.length === set.length).toBe(true);
+  // // });
+
+  // // test("10000000", () => {
+  // //   const arr = Array(10000000)
+  // //     .fill(0)
+  // //     .map(() => IdRandom());
+  // //   const set = [...new Set(arr)];
+  // //   console.log("arr.length === set.length", arr.length === set.length);
+  // //   expect(arr.length === set.length).toBe(true);
+  // // });
 });
