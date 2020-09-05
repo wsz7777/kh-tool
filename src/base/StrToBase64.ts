0061f927861925db110ba14710825ec11927f5aa
王世桢 <wangsz5@aisainfo.com>
2020-09-05 12:37:31 +0800

    增加对字符串加密至非特殊字符串方法

--- src/base/StrToBase64.ts
+++ src/base/StrToBase64.ts
@@ -1,8 +1,20 @@
 import { enc } from "crypto-js";
 const { Base64, Utf8 } = enc;
 
-export const StrToBase64 = (str: string): string =>
-  Base64.stringify(Utf8.parse(str));
+export function StrToBase64(str: string): string {
+  return Base64.stringify(Utf8.parse(str));
+}
 
-export const StrFromBase64 = (str: string): string =>
-  Base64.parse(str).toString(Utf8);
+export function StrFromBase64(str: string): string {
+  return Base64.parse(str).toString(Utf8);
+}
+
+/** 解析为url路径str专用 */
+export function StrToUrlStr(str: string): string {
+  return StrToBase64(str).replace(/\//g, "-").replace(/=/g, "_");
+}
+
+/** 从url路径str解析原意专用 */
+export function StrFromUrlStr(str: string): string {
+  return StrFromBase64(str.replace(/-/g, "/").replace(/_/g, "="));
+}
--- test/base/StrToBase64.test.ts
+++ test/base/StrToBase64.test.ts
@@ -1,13 +1,42 @@
-import { StrFromBase64, StrToBase64 } from "../../src";
+import {
+  StrFromBase64,
+  StrToBase64,
+  StrToUrlStr,
+  StrFromUrlStr,
+} from "../../src";
 
 describe("StrToBase64:", () => {
   test("empty string", () => {
-    expect(StrToBase64("")).toBe("");
+    expect(/\d|\w|\/|(={0,2}$)/g.test(StrToBase64(""))).toBe(true);
+  });
+  test("djsnkdvm,vcxzkvl", () => {
+    expect(/\d|\w|\/|(={0,2}$)/g.test(StrToBase64("djsnkdvm,vcxzkvl"))).toBe(
+      true
+    );
+  });
+  test("·1234567890-=qwertyuiop电竞少女库、，。、？", () => {
+    expect(
+      /\d|\w|\/|(={0,2}$)/g.test(
+        StrToBase64("·1234567890-=qwertyuiop电竞少女库、，。、？")
+      )
+    ).toBe(true);
   });
 });
 
 describe("StrFromBase64:", () => {
   test("empty string", () => {
-    expect(StrFromBase64("===")).toBe("");
+    expect(StrFromBase64("==")).toBe("");
+  });
+});
+
+describe("StrToUrlStr:", () => {
+  test("StrToUrlStr", () => {
+    expect(/[^\d|\w|-|_|+]/g.test(StrToUrlStr("fsdfds"))).toBe(false);
+  });
+});
+
+describe("StrToUrlStr", () => {
+  test("a2wxOS8yM3doJGtsMTkvMjN3aA__", () => {
+    expect(StrFromUrlStr("a2wxOS8yM3doJGtsMTkvMjN3aA__")).toBe('kl19/23wh$kl19/23wh');
   });
 });
