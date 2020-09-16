34b6a6e3633711c16bf8b022b66d145abbec5689
王世桢 <wangsz5@aisainfo.com>
2020-09-16 15:06:49 +0800

    完善名称转换

--- src/base/NameStyle.ts
+++ src/base/NameStyle.ts
@@ -7,26 +7,36 @@
  * lowerCamel 小驼峰命名法 asdFgh
  */
 
-/** 短横线转换为驼峰 */
-export function Kebab2Camel(s) {
-  return s.replace(/-(\w)/g, function (all, letter) {
-    return letter.toUpperCase();
-  });
+/** 短横线转换为驼峰（大驼峰） */
+export function Kebab2Camel(s: string): string {
+  return s.replace(/(^|-)(\w)/g, (all: string, $1: string, $2: string) =>
+    $2.toUpperCase()
+  );
 }
 
-/** 驼峰转换为短横线 */
-export function Camel2Kebab(s) {
-  return s.replace(/([A-Z])/g, "-$1").toLowerCase();
+/** 短横线转换为小驼峰 */
+export function Kebab2lowerCamel(s: string): string {
+  return s.replace(/-(\w)/g, (all: string, $1: string) => $1.toUpperCase());
 }
 
 /** 下划线转换为驼峰 */
-export function Snake2Camel(s) {
-  return s.replace(/_(\w)/g, function (all, letter) {
-    return letter.toUpperCase();
-  });
+export function Snake2Camel(s: string): string {
+  return s.replace(/(^|_)(\w)/g, (all: string, $1: string, $2: string) =>
+    $2.toUpperCase()
+  );
+}
+
+/** 下划线转换为小驼峰 */
+export function Snake2lowerCamel(s: string): string {
+  return s.replace(/_(\w)/g, (all: string, $1: string) => $1.toUpperCase());
+}
+
+/** 驼峰转换为短横线 */
+export function Camel2Kebab(s: string): string {
+  return s.replace(/\w([A-Z])/g, "-$1").toLowerCase();
 }
 
 /** 驼峰转换为下划线 */
-export function Camel2Snake(s) {
-  return s.replace(/([A-Z])/g, "_$1").toLowerCase();
+export function Camel2Snake(s: string): string {
+  return s.replace(/\w([A-Z])/g, "_$1").toLowerCase();
 }
